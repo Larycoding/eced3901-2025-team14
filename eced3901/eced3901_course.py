b1c339af2e89efaae3a90e04b92db968ec5698d8
Larycoding <lr754965@dal.ca>
2025-03-12 16:15:39 -0300

    New file for course navigation

---
+++ eced3901/eced3901_course.py
@@ -0,0 +1,87 @@
+# Course: ECED 3901 Engineering Design II
+# 
+# Authors: Larissa Lemoine, Behnam Nazari, James Hillaby
+# This program is written for the ECED 3901 class robo heist. 
+# The program is written for Group 14.
+# 
+# Robot Name: Pascal
+# Date: March 12, 2025
+#
+# Competition Date: March 20,2025
+#
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+
+# http://docs.ros.org/en/noetic/api/geometry_msgs/html/index-msg.html
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Twist
+
+from sensor_msgs.msg import LaserScan
+
+from collections import deque
+import math
+import time
+#import class from other file
+from eced3901_dt1.py import LaserDataInterface
+
+#New class based on the Navigate square class in eced3901_dt1.py file.
+class NavigateCourse(Node):
+     #initialization taken and edited from navigate square
+     def __init__(self):
+        #This calls the initilization function of the base Node class
+        super().__init__('navigate_square')
+
+        # We can either create variables here by declaring them
+        # as part of the 'self' class, or we could declare them
+        # earlier as part of the class itself. I find it easier
+        # to declare them here
+
+        # Ensure these are obviously floating point numbers and not
+        # integers (python is loosely typed)
+
+        # WARNING: Check for updates, note this is set and will run backwards
+        #          on the physical model but correctly in simulation.
+        self.x_vel = -0.2 # velocity changed to correct value for robot not simulation
+
+        self.x_now = 0.0
+        self.x_init = 0.0
+        self.y_now = 0.0
+        self.y_init = 0.0
+        self.d_now = 0.0
+        self.d_aim = 1.0
+        self.i = 0
+
+        self.laser_range = None
+
+        # Subscribe to odometry
+        self.sub_odom = self.create_subscription(
+            Odometry, #Matches the import at the top (nav_msgs.msg import Odometry)
+            'odom', #odom topic
+            self.odom_callback, #Call this function
+            10) #Queue size 10
+
+        # Publish to velocity
+        self.pub_vel = self.create_publisher(
+            Twist, #Expects the twist message format
+            'cmd_vel',
+            10) #Queue size 10
+
+        # Subscribe to lidar - this requires a policy, otherwise you won't see any data
+        qos_policy = rclpy.qos.QoSProfile(reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
+                                          history=rclpy.qos.HistoryPolicy.KEEP_LAST,
+                                          depth=1)
+
+        self.sub_lidar = self.create_subscription(
+            LaserScan,
+            'scan',
+            self.range_callback,
+            qos_policy
+        )
+
+        self.ldi = LaserDataInterface(logger=self.get_logger())
+
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
